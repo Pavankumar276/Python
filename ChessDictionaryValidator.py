697781ee94c74bece543f0a9978d6429b617fb18
Pavankumar276 <pavankumar.24.beai@acharya.ac.in>
2025-05-22 14:01:45 +0530

    Create ChessDictionaryValidator.py

---
+++ ChessDictionaryValidator.py
@@ -0,0 +1,11 @@
+--- Chess Dictionary ---
+
+Piece: King
+  Abbreviation: K
+  Movement: Moves one square in any direction.
+
+Piece: Queen
+  Abbreviation: Q
+  Movement: Moves any number of squares in any direction.
+
+...
